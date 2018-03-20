79e8067904d5bc4ee7a70a6e60f9eafaac3a96a4
JohnFII <ionuticb@gmail.com>
2018-03-20 08:52:25 +0200

    Add files via upload

---
+++ IP_Lab4/IP_ALEX/Event.java
@@ -0,0 +1,29 @@
+public class Event {
+
+  public String eventType;
+
+  public long time;
+
+  public int resultedCode;
+
+  public String action;
+
+    public EventHandler myEventHandler;
+
+  public String getType() {
+  return null;
+  }
+
+  public long getTime() {
+  return 0;
+  }
+
+  public String getAction() {
+  return null;
+  }
+
+  public String getResultedCode() {
+  return null;
+  }
+
+}
---
+++ IP_Lab4/IP_ALEX/EventHandler.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class EventHandler {
+
+    public Event myEvent;
+    /**
+   * 
+   * @element-type Application
+   */
+  public Vector  myApplication;
+
+  public void setEvent() {
+  }
+
+  public void handle() {
+  }
+
+}
